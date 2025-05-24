94c70698c9471c32d57860b9fad1fbfd68de1707
JonaSaavedra <jonathan.saavedra.gonzalez@gmail.com>
2025-05-24 05:31:57 +0200

    Agregar modal apagado

---
+++ JS/turnoff.js
@@ -0,0 +1,18 @@
+const turnOffWindow = document.querySelector(".turnoff__window");
+
+const cancelShutDown = document.getElementById("cancel").addEventListener("click", () => {
+    
+    turnOffWindow.style.display = "none";
+    document.body.style.backdropFilter = "grayscale(0)";
+
+  });
+
+const shutDownButton = document.querySelector(".shutdown").addEventListener("click", () => {
+
+    turnOffWindow.style.display = "block";
+
+    document.body.style.backdropFilter = "grayscale(0.8)";
+    const taskBar = document.querySelector(".taskbar");
+    taskBar.style.filter = "grayscale(0)";
+
+  });
