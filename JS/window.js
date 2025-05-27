b22af3235b40008eb66a38909787deebe0f50945
JonaSaavedra <jonathan.saavedra.gonzalez@gmail.com>
2025-05-28 01:31:29 +0200

    cri

---
+++ JS/window.js
@@ -0,0 +1,7 @@
+const taskbarWindows = document.querySelector(".taskbar__windows");
+const taskbarWindow = document.createElement("div");
+
+taskbarWindows.appendChild(taskbarWindow);
+
+taskbarWindow.classList.add("taskbar__window")
+
